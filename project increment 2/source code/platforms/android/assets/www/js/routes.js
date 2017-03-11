afffb9f16bb18084efb1f1598deb34def1d89a21
seshu <spcgkd@mail.umkc.edu>
2017-03-11 01:17:37 -0600

    commit

---
+++ project increment 2/source code/platforms/android/assets/www/js/routes.js
@@ -0,0 +1,85 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.home', {
+    url: '/page1',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('menu.remind', {
+    url: '/Remind',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/remind.html',
+        controller: 'remindCtrl'
+      }
+    }
+  })
+
+  .state('menu.alarm', {
+    url: '/Alarm',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/alarm.html',
+        controller: 'alarmCtrl'
+      }
+    }
+  })
+
+  .state('menu.notes', {
+    url: '/notes',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/notes.html',
+        controller: 'notesCtrl'
+      }
+    }
+  })
+
+  .state('menu.about', {
+    url: '/about',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/about.html',
+        controller: 'aboutCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+  
+
+});
